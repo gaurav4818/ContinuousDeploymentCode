b22bc22e28fea1e8b3429ca86bc6942896a3e1e6
gaurav4818 <rathorgaurav4818@gmail.com>
2022-07-16 21:49:11 +0530

    Add project files.

---
+++ ContinuousDeploymentCode/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContinuousDeploymentCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
