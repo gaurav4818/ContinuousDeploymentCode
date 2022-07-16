0314f954e033a00349e5766a4b9c53168d9c1b87
gaurav4818 <rathorgaurav4818@gmail.com>
2022-07-16 23:00:39 +0530

    added azure app configuration

--- ContinuousDeploymentCode/Program.cs
+++ ContinuousDeploymentCode/Program.cs
@@ -1,7 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = "Endpoint=https://appconfig9009.azconfig.io;Id=Yd8n-lh-s0:OSvWr79RTAWgfAi0QZ/S;Secret=Uh3U2Io2Oy3AB+t0X8CU5d84fTWPHtZn7i+goiD01C0=";
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Host.ConfigureAppConfiguration(builder =>
+{
+    //Connect to your App Config Store using the connection string
+    builder.AddAzureAppConfiguration(connectionString);
+})
+            .ConfigureServices(services =>
+            {
+                services.AddControllersWithViews();
+            });
 
 var app = builder.Build();
 
